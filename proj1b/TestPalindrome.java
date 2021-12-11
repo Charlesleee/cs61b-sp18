214403a6203399fd8658c78a77f6490186a5b07e
Charlesleee <1337439316@qq.com>
2021-12-11 23:59:11 +0800

    update

--- proj1b/TestPalindrome.java
+++ proj1b/TestPalindrome.java
@@ -5,6 +5,7 @@ public class TestPalindrome {
     // You must use this palindrome, and not instantiate
     // new Palindromes, or the autograder might be upset.
     static Palindrome palindrome = new Palindrome();
+    static OffByOne cc = new OffByOne();
 
     @Test
     public void testWordToDeque() {
@@ -17,9 +18,23 @@ public class TestPalindrome {
     }
 
     @Test
-    public void TestPalindrome() {
-        assertFalse(palindrome.isPalindrome("sccverg"));
-        assertTrue(palindrome.isPalindrome("aabbaa"));
-        assertFalse(palindrome.isPalindrome("cat"));
+    public void testIsPalindrome() {
+        String a = "abc";
+        assertFalse(palindrome.isPalindrome(a));
+        String b = "b";
+        assertTrue(palindrome.isPalindrome(b));
+        String c = "";
+        assertTrue(palindrome.isPalindrome(c));
+        String d = "dadad";
+        assertTrue(palindrome.isPalindrome(d));
+
+        String a1 = "flake";
+        assertTrue(palindrome.isPalindrome(a1, cc));
+        String b1 = "b";
+        assertTrue(palindrome.isPalindrome(b1, cc));
+        String c1 = "";
+        assertTrue(palindrome.isPalindrome(c1, cc));
+        String d1 = "dadad";
+        assertFalse(palindrome.isPalindrome(d1, cc));
     }
 }
